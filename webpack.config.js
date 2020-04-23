f337cbc3a132662c0fbadbaf306a46f801334649
Christopher Schleiden <cschleiden@github.com>
2020-04-23 16:42:40 -0700

    Support keytar in remote workspaces

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,6 @@ const config = {
   devtool: "source-map",
   externals: {
     vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    keytar: "keytar"
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
