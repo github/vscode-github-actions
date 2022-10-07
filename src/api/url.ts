14190779466cb6a32cfa8752f02561603c5e0c2e
Christopher Schleiden <cschleiden@github.com>
2022-10-06 17:04:53 -0700

    Remove unused code

--- src/api/url.ts
+++
@@ -1,7 +0,0 @@
-import { URL } from "url";
-
-export function workflowsUrl(repository: string): string {
-  const url = new URL(`${repository}/actions/workflows`);
-  url.host = "api.github.com";
-  return url.toString();
-}
