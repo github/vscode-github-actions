f942808a7e03c372e183051be42978eabdf51c29
Jacob Wallraff <thyeggman@github.com>
2022-10-13 10:33:57 -0700

    Fix client linting

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,6 @@
 import {Octokit} from '@octokit/rest';
 import {getClient as getAPIClient} from '../api/api';
 
-export async function getClient(token: string): Promise<Octokit> {
+export function getClient(token: string): Octokit {
   return getAPIClient(token);
 }
