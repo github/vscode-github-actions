da12dc09eed395e7fcce5c8bf66f0a2fa91e9c81
Christopher Schleiden <cschleiden@github.com>
2022-10-19 08:51:32 -0700

    Safe clone repos

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/github/actions-expressions /workspaces/actions-expressions
-git clone https://github.com/github/actions-workflow-parser /workspaces/actions-workflow-parser
-git clone https://github.com/github/actions-languageservice /workspaces/actions-languageservice
-git clone https://github.com/github/actions-languageserver /workspaces/actions-languageserver
+function clone_repo() {
+  GREEN='\033[0;32m'
+
+  REPOSITORY_URL=$1
+  REPOSITORY_PATH=$2
+  if [[ ! -d "$REPOSITORY_PATH/.git" ]]; then
+    echo -e "\n${GREEN}➡️ Cloning $REPOSITORY_URL...\n${NC}"
+    git clone "$REPOSITORY_URL" "$REPOSITORY_PATH"
+  fi
+}
+
+clone_repo https://github.com/github/actions-expressions /workspaces/actions-expressions
+clone_repo https://github.com/github/actions-workflow-parser /workspaces/actions-workflow-parser
+clone_repo https://github.com/github/actions-languageservice /workspaces/actions-languageservice
+clone_repo https://github.com/github/actions-languageserver /workspaces/actions-languageserver
+
