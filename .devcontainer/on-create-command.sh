7e3c50dbb6aeb7703d2199cf50bc3ed676dd8a78
Christopher Schleiden <cschleiden@github.com>
2022-10-19 08:33:14 -0700

    Configure devcontainer

---
+++ .devcontainer/on-create-command.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/github/actions-expressions /workspaces/actions-expressions
+git clone https://github.com/github/actions-workflow-parser /workspaces/actions-workflow-parser
+git clone https://github.com/github/actions-languageservice /workspaces/actions-languageservice
+git clone https://github.com/github/actions-languageserver /workspaces/actions-languageserver
