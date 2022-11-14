4c078f718088538d1ffe24437151af828fe56474
Josh Gross <joshmgross@github.com>
2022-11-14 15:54:29 -0500

    Remove unused function

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-function clone_repo() {
-  GREEN='\033[0;32m'
-
-  REPOSITORY_URL=$1
-  REPOSITORY_PATH=$2
-  if [[ ! -d "$REPOSITORY_PATH/.git" ]]; then
-    echo -e "\n${GREEN}➡️ Cloning $REPOSITORY_URL...\n${NC}"
-    git clone "$REPOSITORY_URL" "$REPOSITORY_PATH"
-  fi
-}
-
 # Cache git credentials
 git config --global credential.helper cache
 git credential-cache exit
