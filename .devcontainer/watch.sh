1e16d664ac72e33a7fe0d830b7c0868f48fab280
Josh Gross <joshmgross@github.com>
2022-11-14 13:30:31 -0500

    Update dev scripts to point to new repo

--- .devcontainer/watch.sh
+++ .devcontainer/watch.sh
@@ -3,7 +3,7 @@
 # allow killing all processes with ctrl-c
 trap 'kill 0' SIGINT
 
-for DIR in "actions-expressions/typescript" "actions-workflow-parser/typescript" "actions-languageservice" "actions-languageserver"
+for DIR in "actions-expressions/typescript" "actions-workflow-parser/typescript" "actions-languageservices/actions-languageservice" "actions-languageservices/actions-languageserver" "vscode-github-actions"
 do
   pushd $DIR
   npm run watch &
