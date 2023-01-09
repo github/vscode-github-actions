8c1ea83a64b54be329399b27b2875500013f9ee2
Christopher Schleiden <cschleiden@github.com>
2023-01-09 15:11:44 -0800

    Update watch

--- .devcontainer/watch.sh
+++ .devcontainer/watch.sh
@@ -3,7 +3,7 @@
 # allow killing all processes with ctrl-c
 trap 'kill 0' SIGINT
 
-for DIR in "actions-expressions/typescript" "actions-workflow-parser/typescript" "actions-languageservices/actions-languageservice" "actions-languageservices/actions-languageserver" "vscode-github-actions"
+for DIR in "actions-languageservices/actions-expressions" "actions-languageservices/actions-workflow-parser" "actions-languageservices/actions-languageservice" "actions-languageservices/actions-languageserver" "vscode-github-actions"
 do
   pushd $DIR
   npm run watch &
