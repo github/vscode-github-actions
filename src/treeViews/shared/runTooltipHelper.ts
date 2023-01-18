bd0f19a853f57e6277ac608dc6cc32e815c2714e
Beth Brennan <elbrenn@github.com>
2023-01-18 15:40:27 -0500

    Use dayjs, PR feedback cleanup

--- src/treeViews/shared/runTooltipHelper.ts
+++ src/treeViews/shared/runTooltipHelper.ts
@@ -1,9 +1,14 @@
-import moment from "moment";
-import "moment-duration-format";
-import {WorkflowRun} from "../../store/workflowRun";
-import {WorkflowRunAttempt} from "../../store/workflowRun";
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat.js";
+import relativeTime from "dayjs/plugin/relativeTime.js";
+import duration from "dayjs/plugin/duration.js";
+import {WorkflowRun, WorkflowRunAttempt} from "../../store/workflowRun";
 
-// Reutrns a string like "**Succeeded** in **1m 2s**"
+dayjs.extend(duration);
+dayjs.extend(localizedFormat);
+dayjs.extend(relativeTime);
+
+// Returns a string like "**Succeeded** in **1m 2s**"
 // For use in markdown tooltip
 export function getStatusString(item: WorkflowRun | WorkflowRunAttempt, capitalize: boolean = false): string {
   let statusText = item.run.conclusion || item.run.status || "";
@@ -25,14 +30,20 @@ export function getStatusString(item: WorkflowRun | WorkflowRunAttempt, capitali
   statusText = `**${statusText}**`;
 
   if (item.run.conclusion && item.run.conclusion !== "skipped") {
-    const duration = moment.duration(item.duration());
-    statusText += ` in **${duration.format("D[d] h[h] m[m] s[s]")}**`;
+    const duration = dayjs.duration(item.duration());
+    // Format and remove leading 0's
+    const formattedDuration = duration
+      .format("D[d] H[h] m[m] s[s]")
+      .replace(/^0d /, "")
+      .replace(/^0h /, "")
+      .replace(/^0m /, "");
+    statusText += ` in **${formattedDuration}**`;
   }
 
   return statusText;
 }
 
-// Reutrns a string like "Manually run by [user](user_url) 4 minutes ago *(December 31, 1969 7:00 PM)*"
+// Returns a string like "Manually run by [user](user_url) 4 minutes ago *(December 31, 1969 7:00 PM)*"
 // For use in markdown tooltip
 export function getEventString(item: WorkflowRun | WorkflowRunAttempt): string {
   let eventString = "Triggered";
@@ -58,8 +69,10 @@ export function getEventString(item: WorkflowRun | WorkflowRunAttempt): string {
     eventString += ` by [${item.run.triggering_actor.login}](${item.run.triggering_actor.html_url})`;
   }
 
-  let started_at = moment(item.run.run_started_at);
-  eventString += ` ${started_at.fromNow()} *(${started_at.format("LLL")})*`;
+  if (item.run.run_started_at) {
+    let started_at = dayjs(item.run.run_started_at);
+    eventString += ` ${started_at.fromNow()} *(${started_at.format("LLL")})*`;
+  }
 
   return eventString;
 }
