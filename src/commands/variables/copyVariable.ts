b2fe3e7cb8b91e61a5caa5d978a711ef6326d2c7
Josh Gross <joshmgross@github.com>
2023-01-27 10:51:49 -0500

    Support copying variables in the sidebar

---
+++ src/commands/variables/copyVariable.ts
@@ -0,0 +1,24 @@
+import * as vscode from "vscode";
+import {VariableCommandArgs} from "../../treeViews/settings/variableNode";
+
+export function registerCopyVariable(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("github-actions.settings.variable.copy-name", async (args: VariableCommandArgs) => {
+      const {variable} = args;
+
+      await vscode.env.clipboard.writeText(variable.name);
+
+      vscode.window.setStatusBarMessage(`Copied ${variable.name}`, 2000);
+    }),
+    vscode.commands.registerCommand(
+      "github-actions.settings.variable.copy-value",
+      async (args: VariableCommandArgs) => {
+        const {variable} = args;
+
+        await vscode.env.clipboard.writeText(variable.value);
+
+        vscode.window.setStatusBarMessage(`Copied ${variable.value}`, 2000);
+      }
+    )
+  );
+}
