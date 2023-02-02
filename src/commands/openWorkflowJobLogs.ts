b2380f06150020c2fed3ae016f3b00ca5aa70eaa
Beth Brennan <elbrenn@github.com>
2023-02-02 16:02:05 -0500

    Remove step links to logs

--- src/commands/openWorkflowJobLogs.ts
+++ src/commands/openWorkflowJobLogs.ts
@@ -9,7 +9,6 @@ import {WorkflowJob} from "../store/WorkflowJob";
 export interface OpenWorkflowJobLogsCommandArgs {
   gitHubRepoContext: GitHubRepoContext;
   job: WorkflowJob;
-  step?: WorkflowStep;
 }
 
 export function registerOpenWorkflowJobLogs(context: vscode.ExtensionContext) {
@@ -17,7 +16,6 @@ export function registerOpenWorkflowJobLogs(context: vscode.ExtensionContext) {
     vscode.commands.registerCommand("github-actions.workflow.logs", async (args: OpenWorkflowJobLogsCommandArgs) => {
       const gitHubRepoContext = args.gitHubRepoContext;
       const job = args.job;
-      const step = args.step;
       const uri = buildLogURI(
         `%23${job.job.run_id} - ${job.job.name}`,
         gitHubRepoContext.owner,
@@ -37,22 +35,6 @@ export function registerOpenWorkflowJobLogs(context: vscode.ExtensionContext) {
 
       // Custom formatting after the editor has been opened
       updateDecorations(editor, logInfo);
-
-      // Deep linking to steps
-      if (step) {
-        let matchingSection = logInfo.sections.find(s => s.name && s.name === step.name);
-        if (!matchingSection) {
-          // If we cannot match by name, see if we can try to match by number
-          matchingSection = logInfo.sections[step.number - 1];
-        }
-
-        if (matchingSection) {
-          editor.revealRange(
-            new vscode.Range(matchingSection.start, 0, matchingSection.start, 0),
-            vscode.TextEditorRevealType.InCenter
-          );
-        }
-      }
     })
   );
 }
