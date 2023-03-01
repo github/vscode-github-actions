0446416f32354d675cc8df8086a2ffbbafe45b72
Laura Yu <60276246+lauraway@users.noreply.github.com>
2023-03-01 10:31:31 -0800

    Add message for deletion verification (#96)

--- src/commands/secrets/deleteSecret.ts
+++ src/commands/secrets/deleteSecret.ts
@@ -6,13 +6,26 @@ export function registerDeleteSecret(context: vscode.ExtensionContext) {
     vscode.commands.registerCommand("github-actions.settings.secret.delete", async (args: SecretCommandArgs) => {
       const gitHubContext = args.gitHubRepoContext;
       const secret = args.secret;
-
-      await gitHubContext.client.actions.deleteRepoSecret({
-        owner: gitHubContext.owner,
-        repo: gitHubContext.name,
-        secret_name: secret.name
-      });
-
+      const acceptText = "Yes, delete this secret";
+      try {
+        await vscode.window
+          .showInformationMessage(
+            `Are you sure you want to delete ${secret.name}?`,
+            {modal: true, detail: "Deleting this secret cannot be undone and may impact workflows in this repository"},
+            acceptText
+          )
+          .then(async answer => {
+            if (answer === acceptText) {
+              await gitHubContext.client.actions.deleteRepoSecret({
+                owner: gitHubContext.owner,
+                repo: gitHubContext.name,
+                secret_name: secret.name
+              });
+            }
+          });
+      } catch (e) {
+        await vscode.window.showErrorMessage((e as Error).message);
+      }
       await vscode.commands.executeCommand("github-actions.explorer.refresh");
     })
   );
--- src/commands/variables/deleteVariable.ts
+++ src/commands/variables/deleteVariable.ts
@@ -5,23 +5,36 @@ export function registerDeleteVariable(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("github-actions.settings.variable.delete", async (args: VariableCommandArgs) => {
       const {gitHubRepoContext, variable, environment} = args;
+      const acceptText = "Yes, delete this variable";
 
       try {
-        if (environment) {
-          await gitHubRepoContext.client.request(
-            `DELETE /repositories/${gitHubRepoContext.id}/environments/${environment.name}/variables/${variable.name}`
-          );
-        } else {
-          await gitHubRepoContext.client.actions.deleteRepoVariable({
-            owner: gitHubRepoContext.owner,
-            repo: gitHubRepoContext.name,
-            name: variable.name
+        await vscode.window
+          .showInformationMessage(
+            `Are you sure you want to delete ${variable.name}?`,
+            {
+              modal: true,
+              detail: "Deleting this variable cannot be undone and may impact workflows in this repository"
+            },
+            acceptText
+          )
+          .then(async answer => {
+            if (answer === acceptText) {
+              if (environment) {
+                await gitHubRepoContext.client.request(
+                  `DELETE /repositories/${gitHubRepoContext.id}/environments/${environment.name}/variables/${variable.name}`
+                );
+              } else {
+                await gitHubRepoContext.client.actions.deleteRepoVariable({
+                  owner: gitHubRepoContext.owner,
+                  repo: gitHubRepoContext.name,
+                  name: variable.name
+                });
+              }
+            }
           });
-        }
       } catch (e) {
         await vscode.window.showErrorMessage((e as Error).message);
       }
-
       await vscode.commands.executeCommand("github-actions.explorer.refresh");
     })
   );
