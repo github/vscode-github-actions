4d5cae9bd642f8cb4a7aa9ceda34bf5df75790f4
Crystal Tenn <crtenn@microsoft.com>
2023-04-06 18:16:55 -0400

    Set all contexts for view upon sign in
    Co-authored-by: Liela Rotschy <lrotschy@github.com>

--- src/commands/signIn.ts
+++ src/commands/signIn.ts
@@ -1,12 +1,20 @@
 import * as vscode from "vscode";
 import {getSession} from "../auth/auth";
+import {canReachGitHubAPI} from "../api/canReachGitHubAPI";
+import {getGitHubContext} from "../git/repository";
 
 export function registerSignIn(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("github-actions.sign-in", async () => {
       const session = await getSession(true);
       if (session) {
+        const canReachAPI = await canReachGitHubAPI();
+        const ghContext = await getGitHubContext();
+        const hasGitHubRepos = ghContext && ghContext.repos.length > 0;
+
         await vscode.commands.executeCommand("setContext", "github-actions.signed-in", true);
+        await vscode.commands.executeCommand("setContext", "github-actions.internet-access", canReachAPI);
+        await vscode.commands.executeCommand("setContext", "github-actions.has-repos", hasGitHubRepos);
       }
     })
   );
