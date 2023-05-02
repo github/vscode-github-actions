a9311062b1ea0512c126bccd900cc48c483fd5b1
Crystal Tenn <crtenn@microsoft.com>
2023-05-02 12:56:51 -0400

    wip to be able to open a web page with the specific step
    Co-authored-by: Liela Rotschy <lrotschy@github.com>

--- src/commands/openWorkflowStepLogs.ts
+++ src/commands/openWorkflowStepLogs.ts
@@ -1,55 +1,32 @@
 import * as vscode from "vscode";
+import {WorkflowJob, WorkflowRun} from "../model";
 import {GitHubRepoContext} from "../git/repository";
-import {updateDecorations} from "../logs/formatProvider";
-import {getLogInfo} from "../logs/logInfo";
-import {buildLogURI} from "../logs/scheme";
-import {WorkflowJob} from "../store/WorkflowJob";
-import { WorkflowStep } from "../model";
+import {integer} from "vscode-languageclient";
+import {WorkflowRunCommandArgs, WorkflowRunNode} from "../treeViews/shared/workflowRunNode";
+import {WorkflowJobNode} from "../treeViews/shared/workflowJobNode";
 
-export interface OpenWorkflowStepLogsCommandArgs {
-  gitHubRepoContext: GitHubRepoContext;
-  job: WorkflowJob;
-  step: WorkflowStep;
-}
+// export interface OpenWorkflowStepLogsCommandArgs {
+//   gitHubRepoContext: GitHubRepoContext;
+//   job: WorkflowJob;
+//   run: WorkflowRun;
+// }
+
+export type WorkflowStepCommandArgs = Pick<WorkflowJobNode, "job">;
 
 export function registerOpenWorkflowStepLogs(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("github-actions.step.logs", async (args: OpenWorkflowStepLogsCommandArgs) => {
-      const gitHubRepoContext = args.gitHubRepoContext;
-      const job = args.job;
-      const step = args.step;
+    vscode.commands.registerCommand("github-actions.step.logs", async (args: WorkflowStepCommandArgs) => {
+      const job = args.job.job;
+      let url = job.html_url ?? "";
+      const stepName = args.step.name;
 
-      const uri = buildLogURI(
-        `%23${job.job.run_id} - ${job.job.name}`,
-        gitHubRepoContext.owner,
-        gitHubRepoContext.name,
-        job.job.id
-      );
+      const index = job.steps && job.steps.findIndex(step => step.name === stepName) + 1;
 
-      const doc = await vscode.workspace.openTextDocument(uri);
-      const editor = await vscode.window.showTextDocument(doc, {
-        preview: false
-      });
-
-      const logInfo = getLogInfo(uri);
-      if (!logInfo) {
-        throw new Error("Could not get log info");
+      if (url && index) {
+        url = url + "#step:" + index.toString() + ":1";
       }
 
-      const section = logInfo.sections.find(s => s.name === step.name);
-      const startLine = section ? section.start : 0;
-      const endLine = section ? section.end : 0;
-
-
-      const stepInfo = {
-        updatedLogLines: logInfo["updatedLogLines"].slice(startLine, endLine),
-        sections: logInfo["sections"].filter(s => s.name === step.name),
-        styleFormats: logInfo["styleFormats"].filter(s => s.line >= startLine && s.line <= endLine)
-      };
-
-
-      // Custom formatting after the editor has been opened
-      updateDecorations(editor, stepInfo);
+      await vscode.env.openExternal(vscode.Uri.parse(url));
     })
   );
 }
