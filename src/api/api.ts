029ecba92ef97051f3cedf64405aefd86ec3a733
flmeyer <florian.meyer@capgemini.com>
2023-05-12 21:04:41 +0200

    Remove unnecessary enterprise server plugin

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,13 @@
 import {Octokit} from "@octokit/rest";
-import {enterpriseServer34} from "@octokit/plugin-enterprise-server";
 import {version} from "../../package.json";
-import {getGitHubApiUri, isUseEnterprise} from "../configuration/configuration";
+import {getGitHubApiUri} from "../configuration/configuration";
 
 export const userAgent = `VS Code GitHub Actions (${version})`;
 
-export const OctokitES = Octokit.plugin(enterpriseServer34);
-
 export function getClient(token: string): Octokit {
-  return isUseEnterprise()
-    ? new OctokitES({
-        auth: token,
-        userAgent: userAgent,
-        baseUrl: `${getGitHubApiUri()}`
-      })
-    : new Octokit({
-        auth: token,
-        userAgent: userAgent
-      });
+  return new Octokit({
+    auth: token,
+    userAgent: userAgent,
+    baseUrl: `${getGitHubApiUri()}`
+  });
 }
